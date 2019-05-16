dd9458bf7390d647bd1733c8cb5cd1f17d741ac2
Anton Kushnikov <a.kushnikov.sas@gmail.com>
2019-05-16 17:33:10 +0500

    removed Green Elephant, added JOIN instead of getAuthorNameById, handled messy concatenations

--- test_task_intern.php
+++ test_task_intern.php
@@ -14,7 +14,6 @@
 */
 
 //устанавливаем соединение с базой данных
-error_reporting(-1);
 $dbh = new PDO('mysql:host=localhost;dbname=test;charset=UTF8', 'root', 'y@3#XUC5');
 
 //создаем таблицу авторов
@@ -32,9 +31,11 @@ if ($dbh->exec($authorsTableSql) === false) {
 }
 
 //очищаем старые данные и добавляем несколько авторов для проверки
-$dbh->exec('delete from ' . $authorsTableName);
-$dbh->exec('insert into ' . $authorsTableName . ' (id, name) values (1, "А. Азимов")');
-$dbh->exec('insert into ' . $authorsTableName . ' (id, name) values (2, "Р. Брэдбери")');
+// Чтобы при очистке старых данных он сбрасывался, нужно вместо DELETE делаать TRUNCATE
+$dbh->exec('TRUNCATE TABLE ' . $authorsTableName);
+// Тогда и незачем писать id вручную -- auto_increment же
+$dbh->exec('insert into ' . $authorsTableName . ' (name) values ("А. Азимов")');
+$dbh->exec('insert into ' . $authorsTableName . ' (name) values ("Р. Брэдбери")');
 
 //создаем таблицу с книгами
 $booksTableName = 'books';
@@ -57,63 +58,50 @@ if ($dbh->exec($booksTableSql) === false) {
 $dbh->exec('delete from ' . $booksTableName);
 $dbh->exec('insert into ' . $booksTableName . ' (author_id, title, isbn, is_in_stock) values (1, "Я, робот", "5-699-13798-5", 1)');
 $dbh->exec('insert into ' . $booksTableName . ' (author_id, title, isbn, is_in_stock) values (2, "Вино из одуванчиков", "978-5-699-37382-6", 0)');
-$dbh->exec('insert into ' . $booksTableName . ' (author_id, title, isbn, is_in_stock) values (3, "Зелёный слоник", "777-5-699-37382-6", 0)');
 ?>
 
 
 <?php
-//решение в этом блоке
+// MySQL 8.0.16 (legacy authentication), PHP 7.1.19
+// решение в этом блоке
 
-//возвращает имя автора из таблицы авторов по id, взятому из таблицы книг
-function getAuthorNameById($dbh, $authorsTableName, $id){
-    $authorNameSql = 'select id, name from ' . $authorsTableName . ' where id = ' . $id;
-    $response = $dbh->prepare($authorNameSql);
-    $response->execute();
+// добавляет в html-документ строки таблицы
+function getTableRows($dbh, $booksTableName, $authorsTableName){
+    $joinTablesSql = <<<EOL
+    SELECT *
+    FROM
+        {$booksTableName}
+        LEFT OUTER JOIN 
+        {$authorsTableName}
+            ON {$booksTableName}.author_id = {$authorsTableName}.id;
+EOL;
 
-    $result = $response->fetch(PDO::FETCH_ASSOC);
-    return $result['name'] ?? 'Н/Д';
-}
+    $result = "";
+    foreach ($dbh->query($joinTablesSql) as $row) {
+        $isInStock = $row['is_in_stock'] ? "Да" : "Нет";
+        // возможно будет полезным сделать так на случай, если автора этой книги не окажется в таблице авторов.
+        // не знаю, правда, насколько возможен такой случай
+        $name = $row['name'] ?? "Н/Д";
 
-// приводит значение соответствующей колонки к человекочитаемому виду
-function isInStock($flag){
-    return $flag ? 'Да' : 'Нет';
-}
-
-//добавляет в html-документ строки таблицы
-function echoTableRows($dbh, $booksTableName, $authorsTableName){
-    $tableRowSql = 'select * from ' . $booksTableName;
-    foreach ($dbh->query($tableRowSql) as $row) {
-        echo '<tr>';
-        echo '<td class="tableBorder">' . $row['title'] .
-            '<td class="tableBorder">' . $row['isbn'] . '</td>' .
-            '<td class="tableBorder">' . getAuthorNameById($dbh, $authorsTableName, $row['author_id']) . '</td>' .
-            '<td class="tableBorder">' . isInStock($row['is_in_stock']) . '</td>';
-        echo '</tr>';
+        $result .=  "<tr>" .
+            "<td>{$row['title']}</td>" .
+            "<td>{$row['isbn']}</td>" .
+            "<td>{$name}</td>" .
+            "<td>{$isInStock}</td>" .
+            "</tr>";
     }
+    return $result;
 }
+
+$tableRows = getTableRows($dbh, $booksTableName, $authorsTableName);
 ?>
 
-<html>
-<head>
-    <style>
-        .tableBorder{
-            border: 1px solid black;
-        }
-    </style>
-</head>
-<body>
-<table class="tableBorder">
+<table>
     <tr>
         <th class="table">Название</th>
         <th class="table">ISBN</th>
         <th class="table">Автор</th>
         <th class="table">В наличии</th>
     </tr>
-    <?php
-        echoTableRows($dbh, $booksTableName, $authorsTableName);
-    ?>
+    <?=$tableRows?>
 </table>
-</body>
-</html>
-
-
